a2a9edde50777138fd75390ab3eeca27bb1c7e2f
Arthur Coachman <arthur.coachman@transfero.com>
2024-05-11 11:30:05 -0300

    wip

---
+++ RiskFunctions.jl
@@ -0,0 +1,13 @@
+function mean_of_lowest_values(matrix::Matrix{T}) where T<:Real
+    num_rows, num_cols = size(matrix)
+    num_values_to_select = round(Int, 0.05 * num_rows)
+    means = similar(matrix, Float64, num_cols)
+
+    for j in 1:num_cols
+        sorted_col = sort(matrix[:, j])
+        lowest_values = sorted_col[1:num_values_to_select]
+        means[j] = mean(lowest_values)
+    end
+
+    return means
+end
